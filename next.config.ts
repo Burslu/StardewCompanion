054bb36b62858e9070cab44e26dce2753efdab14
Burslu <iberkayuslu41@gmail.com>
2025-12-05 21:24:28 +0300

    fix next cnfig

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
-  output: 'standalone',
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
+  output: 'export',
 };
 
-register: true,
-  skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-      // @ts-ignore
-      buildExcludes: [/middleware-manifest\.json$/],
-}) (nextConfig);
+export default nextConfig;
